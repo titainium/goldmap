d06a7235fdab55eed91d5e5bc4b049f2a630d7ab
Titainium <knifewolf@126.com>
2017-01-17 22:37:15 +0800

    optimize the code with split into functions

--- newscrawler/jinten.py
+++ newscrawler/jinten.py
@@ -19,7 +19,9 @@ import aiohttp
 import asyncpg
 import uvloop
 
+from config import IMAGE_DOWNLOAD_DIR
 from config import JINTEN_LOGGER
+from config import JINTEN_PREFIX
 
 async def fetch_html(session, url):
     try:
@@ -37,67 +39,83 @@ async def fetch_image(session, url):
     except Exception:
         JINTEN_LOGGER.exception("jinten.fetch_image_messages")
 
-async def main(loop):
+async def get_site(conn_pool):
     try:
-        conn_pool = await asyncpg.create_pool(database = 'webCrawler', user = 'titainium')
-
         async with conn_pool.acquire() as connection:
             async with connection.transaction():
                 result = await connection.fetchrow("""select * from target_sites where id = 1""")
         
-        async with aiohttp.ClientSession(loop = loop) as session:
-            relay_point = result['relay_point']
-            html = await fetch_html(session, result['target_url'] + relay_point)
-            insect_food = pq(html, parser = 'html')
-            raw_material = json.dumps({'title': insect_food(".jin-news-article_h").html(),
-                                       'description': insect_food(".jin-news-article_description").html(),
-                                       'content': insect_food(".jin-news-article_content").html()})
-            semi_finished_goods = json.dumps({'semi': insect_food(".jin-news-article_content").text()})
-            images = insect_food(".jin-news-article_content").find("img")
-            download_files = []
+        return result['target_url'], result['relay_point']
+    except Exception:
+        JINTEN_LOGGER.exception("jinten.get_site_messages")
 
-            if not os.path.isdir(os.path.join(os.getcwd(), "download_imgs", "jin10", date.today().isoformat())):
-                os.makedirs(os.path.join(os.getcwd(), "download_imgs", "jin10", date.today().isoformat()))
-            
-            for img in images:
-                with open(os.path.join(os.getcwd(),
-                                       "download_imgs",
-                                       "jin10",
-                                       date.today().isoformat(),
-                                       img.attrib["src"].split("/")[-1]),
-                          "wb") as image_file:
-                    image_file.write(await fetch_image(session, img.attrib["src"]))
-                    download_files.append((os.path.join(os.getcwd(),
-                                                        "download_imgs",
-                                                        "jin10",
-                                                        date.today().isoformat(),
-                                                        img.attrib["src"].split("/")[-1]),
-                                           img.attrib["src"]))
+def get_material(html):
+    try:
+        insect_food = pq(html, parser = 'html')
+        raw_material = json.dumps({'title': insect_food(".jin-news-article_h").html(),
+                                   'description': insect_food(".jin-news-article_description").html(),
+                                   'content': insect_food(".jin-news-article_content").html()})
+        semi_finished_goods = json.dumps({'semi': insect_food(".jin-news-article_content").text()})
+        images = insect_food(".jin-news-article_content").find("img")
 
+        return raw_material, semi_finished_goods, images
+    except Exception:
+        JINTEN_LOGGER.exception("jinten.get_material_messages")
+
+def save_images(session, images):
+    try:
+        download_files = []
+        
+        if not os.path.isdir(os.path.join(os.getcwd(), IMAGE_DOWNLOAD_DIR, JINTEN_PREFIX, date.today().isoformat())):
+            os.makedirs(os.path.join(os.getcwd(), IMAGE_DOWNLOAD_DIR, JINTEN_PREFIX, date.today().isoformat()))
+        
+        for img in images:
+            with open(os.path.join(os.getcwd(), IMAGE_DOWNLOAD_DIR, JINTEN_PREFIX, date.today().isoformat(), img.attrib["src"].split("/")[-1]), "wb") as image_file:
+                image_file.write(await fetch_image(session, img.attrib["src"]))
+                download_files.append((os.path.join(os.getcwd(), IMAGE_DOWNLOAD_DIR, JINTEN_PREFIX, date.today().isoformat(), img.attrib["src"].split("/")[-1]), img.attrib["src"]))
+        
+        return download_files
+    except Exception:
+        JINTEN_LOGGER.exception("jinten.get_material_messages")
+
+async def store_material(conn_pool, raw_material, semi_finished_goods, relay_point):
+    try:
         async with conn_pool.acquire() as connection:
             async with connection.transaction():
-                result = await connection.execute("""INSERT INTO jin10.insect_foods(raw_material,
-                                                                                     semi_finished_goods,
-                                                                                     raw_id)
-                                                     VALUES ($1, $2, $3);""",
+                result = await connection.execute("""INSERT INTO jin10.insect_foods(raw_material, semi_finished_goods, raw_id) VALUES ($1, $2, $3);""",
                                                   raw_material,
                                                   semi_finished_goods,
                                                   relay_point
                                                   )
                 raw_id = await connection.fetchrow("""SELECT id FROM jin10.insect_foods WHERE raw_id = $1;""", relay_point)
         
+        return raw_id['id']
+    except Exception:
+        JINTEN_LOGGER.exception("jinten.store_material_messages")
+
+async def store_images(conn_pool, image_files, raw_id):
+    try:
         async with conn_pool.acquire() as connection:
             async with connection.transaction():
-                for img_file in download_files:
-                    img_result = await connection.execute("""INSERT INTO jin10.complementary_foods(image_name,
-                                                                                                   image_address,
-                                                                                                   stored_path,
-                                                                                                   food_id)
-                                                             VALUES ($1, $2, $3, $4)""",
-                                                          img_file[0].split("/")[-1],
-                                                          img_file[1],
-                                                          img_file[0],
-                                                          raw_id['id'])
+                for img_file in image_files:
+                    await connection.execute("""INSERT INTO jin10.complementary_foods(image_name, image_address, stored_path, food_id) VALUES ($1, $2, $3, $4)""",
+                                             img_file[0].split("/")[-1],
+                                             img_file[1],
+                                             img_file[0],
+                                             raw_id
+                                             )
+    except Exception:
+        JINTEN_LOGGER.exception("jinten.store_images_messages")
+
+async def main(loop):
+    try:
+        conn_pool = await asyncpg.create_pool(database = 'webCrawler', user = 'titainium')
+        
+        async with aiohttp.ClientSession(loop = loop) as session:
+            target_url, relay_point = await get_site(conn_pool)
+            raw_material, semi_finished_goods, images = get_material(await fetch_html(session, target_url + relay_point))
+            raw_id = await store_material(conn_pool, raw_material, semi_finished_goods, relay_point)
+            await store_images(conn_pool, save_images(session, images), raw_id)
     except Exception:
         JINTEN_LOGGER.exception("messages")
 
